1c8b81141a79b93d4b09352e91531c507a9741e3
dcesari <dcesari@c3fd1860-112b-4cc8-9c2e-0324fe32060e>
2011-07-22 09:10:53 +0000

    License commented.

--- libfortrangis/shapelib.F90
+++ libfortrangis/shapelib.F90
@@ -1,20 +1,20 @@
-    Copyright 2011 Davide Cesari <dcesari69 at gmail dot com>
-
-    This file is part of FortranGIS.
-
-    FortranGIS is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
-
-    FortranGIS is distributed in the hope that it will be useful, but
-    WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public
-    License along with FortranGIS.  If not, see
-    <http://www.gnu.org/licenses/>.
+!    Copyright 2011 Davide Cesari <dcesari69 at gmail dot com>
+!
+!    This file is part of FortranGIS.
+!
+!    FortranGIS is free software: you can redistribute it and/or modify
+!    it under the terms of the GNU Lesser General Public License as
+!    published by the Free Software Foundation, either version 3 of the
+!    License, or (at your option) any later version.
+!
+!    FortranGIS is distributed in the hope that it will be useful, but
+!    WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+!    Lesser General Public License for more details.
+!
+!    You should have received a copy of the GNU Lesser General Public
+!    License along with FortranGIS.  If not, see
+!    <http://www.gnu.org/licenses/>.
 
 !> Fortran 2003 interface to the shapelib http://shapelib.maptools.org/
 !! library.
