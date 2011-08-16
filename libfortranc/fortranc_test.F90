6e44c93a9d4933f0b1e771a6f2d069976f3d02e0
dcesari <dcesari@c3fd1860-112b-4cc8-9c2e-0324fe32060e>
2011-08-16 10:22:17 +0000

    Documentation update.

--- libfortranc/fortranc_test.F90
+++ libfortranc/fortranc_test.F90
@@ -13,20 +13,24 @@ END INTERFACE
 TYPE(charpp) :: envp
 INTEGER :: i
 
-! get a char** object from C, declared as
-! { "first", "segundo", "troisieme", NULL }
+! get a char** object from C function return_charpp(), the function
+! result has been declared as:
+! char* var[4] = { "first", "segundo", "troisieme", NULL }
 PRINT*,'Getting a charpp object from C'
 envp = charpp_new(return_charpp())
 !IF (.NOT.C_ASSOCIATED(envp)) THEN
 !  PRINT*,'Error in charpp_new, got a NULL pointer'
 !ENDIF
 
+! get the number of valid pointers in envp
 PRINT*,'The object has ',charpp_getsize(envp),' elements'
 IF (charpp_getsize(envp) /= 3) THEN
   PRINT*,'Error in charpp_getsize:',3,charpp_getsize(envp)
   STOP 1
 ENDIF
 
+! get the content of selected pointers as a Fortran CHARACTER variable
+! of the right length, count starts from 1
 IF (strtofchar(charpp_getptr(envp, 1)) /= 'first') THEN
   PRINT*,'Error in charpp_getptr:',strtofchar(charpp_getptr(envp, 1)),':first'
   STOP 1
