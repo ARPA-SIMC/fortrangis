13bb0f256ac1622df861d907539e1d90b0a1910c
Davide Cesari <dcesari@arpa.emr.it>
2016-03-01 14:57:28 +0100

    revised download page

---
+++ sourceforge/download.php
@@ -0,0 +1,80 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+   <title>FortranGIS - linking Fortran and GIS libraries</title>
+   <link href="style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+
+<div id="page_wrapper">
+
+<div id="header_wrapper">
+
+<div id="header">
+
+<h1>Fortran<font color="#FFDF10">GIS</font></h1>
+<h2>Accessing GIS datasets from Fortran programs</h2>
+
+</div>
+
+<div id="navcontainer">
+<!-- <div class="rl2"> -->
+<ul id="navlist">
+<li><a href="index.php">Home</a></li>
+<li><a href="https://github.com/dcesari/fortrangis">Summary</a></li>
+<li id="active"><a href="#" id="current">Download</a></li>
+<li><a href="https://github.com/dcesari/fortrangis">Browse&nbsp;source</a></li>
+<li><a href="doc/index.html">Documentation</a></li>
+</ul>
+</div>
+
+</div>
+
+<div id="left_side">
+
+<?php
+echo '<a href="http://sourceforge.net/projects/fortrangis"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=603237&amp;type=15" width="150" height="40" alt="Get FortranGIS at SourceForge.net. Fast, secure and Free Open Source software downloads" /></a>';
+?>
+
+<h3>News from the project</h3>
+
+<p>
+<?php
+if ( file_exists("projnews.html") ) {
+        include("projnews.html");
+}
+?>
+</p>
+
+</div>
+
+
+<div id="content">
+
+<h3>FortranGIS download</h3>
+
+<p>You can download a raw source package
+from <a href="https://github.com/dcesari/fortrangis/releases">Github
+release page</a> or
+from <a href="http://sourceforge.net/projects/fortrangis/files/fortrangis/">Sourceforge
+files page.</a></p>
+
+</div>
+
+<div id="footer">
+<!-- <a href="#">Link One</a> |  -->
+<!-- <a href="#">Link Two</a> |  -->
+<!-- <a href="#">Link Three</a> |  -->
+<!-- <a href="#">Link Four</a> |  -->
+<!-- <a href="#">Link Five</a> -->
+<!-- <br /> -->
+FortranGIS by Davide Cesari, e-mail <font color="#FFFFFF">dcesari69 at
+gmail.com</font><br />
+Web page template by Darren Hester, <a
+href="http://www.designsbydarren.com">DesignsByDarren.com</a> </div>
+</div>
+
+</body>
+
+</html>
