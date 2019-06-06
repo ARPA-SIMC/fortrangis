33eb144c872cb909e2df0c6556211592960cf0b8
Emanuele Di Giacomo <emanuele@digiacomo.cc>
2019-06-06 09:47:33 +0200

    Updated travis build

--- .travis-build.sh
+++ .travis-build.sh
@@ -19,10 +19,10 @@ then
     pkgcmd="dnf"
     builddep="dnf builddep"
     sed -i '/^tsflags=/d' /etc/dnf/dnf.conf
-    dnf install -y @buildsys-build
+    dnf install --allowerasing -q -y @buildsys-build
     dnf install -y 'dnf-command(builddep)'
     dnf install -y git
-    dnf copr enable -y pat1/simc
+    dnf copr enable -y simc/stable
 fi
 
 $builddep -y fedora/SPECS/fortrangis.spec
